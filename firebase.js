b80202f072408b7754c0284b2f9a9f8aa492fa2c
Pariisway <quickhands.us@gmail.com>
2025-10-28 22:43:18 -0500

    Update Firebase storage bucket URL

--- firebase.js
+++ firebase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyC7G9FFEZVFRx8Ml_H49tJTcMUbmOZYmQ4",
   authDomain: "iaiwaf.firebaseapp.com",
   projectId: "iaiwaf",
-  storageBucket: "iaiwaf.firebasestorage.app",
+  storageBucket: "iaiwaf.appspot.com",
   messagingSenderId: "548868801867",
   appId: "1:548868801867:web:a51be7e24ea9f1014d94ce"
 };
